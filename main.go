a937e17048c1882a3d2fee344831b6371e8bb1ee
Niko Noll <niko.noll@xing.com>
2017-01-30 19:22:07 +0100

    Urfave/cli setup, defining some flags

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/urfave/cli"
+)
+
+func main() {
+	var city string
+	var start string
+	var destination string
+
+	app := cli.NewApp()
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "c",
+			Value:       "Munich, Germany",
+			Usage:       "current city",
+			Destination: &city,
+		},
+		cli.StringFlag{
+			Name:        "s",
+			Value:       "Goethestr 2",
+			Usage:       "current city",
+			Destination: &start,
+		},
+		cli.StringFlag{
+			Name:        "d",
+			Value:       "Arcisstr 1",
+			Usage:       "Destination adress",
+			Destination: &destination,
+		},
+	}
+
+	app.Action = func(c *cli.Context) error {
+		if c.NArg() > 0 {
+			city = c.Args()[0]
+			start = c.Args()[0]
+			destination = c.Args()[0]
+		}
+		// geovalide adresses
+
+		// call forecast.io on city
+
+		// display weather with wego?
+
+		// call maps api for distance and travel time
+
+		// google compare to alternative travel time (bus etc.)
+
+		fmt.Println("These args were passed", start, destination, city)
+		return nil
+	}
+
+	app.Run(os.Args)
+}
